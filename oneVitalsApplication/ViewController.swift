f1b0390e9c270fd8c7d8b45b762979819158f141
stephen holland <stephenholland279@gmail.com>
2020-12-29 01:25:04 +0000

    Initial Commit

---
+++ oneVitalsApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  oneVitalsApplication
+//
+//  Created by stephen holland on 29/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
